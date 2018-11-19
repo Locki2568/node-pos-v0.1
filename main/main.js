8102f0ac8d2dfe36f0e1505b1aca39a0dd5d70bf
Locki Lo <locki.lo@cargosmart.com>
2018-11-19 17:47:59 +0800

    not yet complete

--- main/main.js
+++ main/main.js
@@ -1,4 +1,30 @@
-module.exports = function main() {
+module.exports = function main(input) {
+
+	var result ='';
+	result = printInventory(input);
+
     console.log("Debug Info");
-    return 'Hello World!';
+    return result;
 };
+
+function printInventory(input){
+		var header = '***<store earning no money>Receipt ***\n';
+		var items = '';
+
+		var distinctItem[] = findDistinctItem(input);
+
+		
+
+		return header + items;
+}
+
+function findDistinctItem(input){
+	var distinctItem[];
+	for(var i=0; i<input.length; i++){
+			var items = input[i];
+			if (!distinctItem.includes(input.Barcode)){
+				distinctItem.push(input.Barcode);
+			}
+	}
+	return distinctItem;
+}
